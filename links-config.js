810825c098c3f6df434438f9a2ceeddcbc8dfcae
Hokage-xMinato <minatonamikaze4756@gmail.com>
2025-04-14 21:42:08 +0530

    Add files via upload

---
+++ links-config.js
@@ -0,0 +1,10 @@
+
+const classLinksData = {
+    'Class 1': "YOUR_CLASS_ONE_LINK_HERE",
+    'Class 2': "YOUR_CLASS_TWO_LINK_HERE",
+    'Class 3': "YOUR_CLASS_THREE_LINK_HERE",
+    'Class 4': "YOUR_CLASS_FOUR_LINK_HERE",
+    // Add more classes and their URLs here
+};
+
+
