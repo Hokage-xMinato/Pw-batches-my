18df906b7f54cb04464cfd61f51b517453591676
Hokage-xMinato <minatonamikaze4756@gmail.com>
2025-04-14 21:45:17 +0530

    Update links-config.js

--- links-config.js
+++ links-config.js
@@ -1,6 +1,6 @@
 
 const classLinksData = {
-    'Class 1': "YOUR_CLASS_ONE_LINK_HERE",
+    'Class 1': "https://www.pw.live/watch/?batchSlug=676e4dee1ec923bc192f38c9&batchSubjectId=676fe65d347e0478eccf9885&subjectSlug=676fe65d347e0478eccf9885&topicSlug=all&scheduleId=67fcfe334277708395f9ee7e&isUnderMaintenance=false&doubtTime=0&type=penpencilvdo&learn2Earn=true&entryPoint=BATCH_TODAYS_CLASS_VIDEOS_634fb54c0c56610011d10202",
     'Class 2': "YOUR_CLASS_TWO_LINK_HERE",
     'Class 3': "YOUR_CLASS_THREE_LINK_HERE",
     'Class 4': "YOUR_CLASS_FOUR_LINK_HERE",
